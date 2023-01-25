0713dd6b5bb48392a6052694a4c70e9a51aed54f
Isaac <42537694+ozervesh@users.noreply.github.com>
2023-01-25 15:38:22 +0800

    Add missions 3 and 4, tutorial 1 files

---
+++ Missions/missions/mission03/mission03-template.py
@@ -0,0 +1,260 @@
+#
+# CS1010X --- Programming Methodology
+#
+# Mission 3
+#
+# Note that written answers are commented out to allow us to run your
+# code easily while grading your problem set.
+
+###########
+# Task 1a #
+###########
+
+def compose(f, g):
+    return lambda x:f(g(x))
+
+def thrice(f):
+    return compose(f, compose(f, f))
+
+def repeated(f, n):
+    if n == 0:
+        return identity
+    else:
+        return compose(f, repeated(f, n - 1))
+
+# Your answer here:
+# n = 9
+
+###########
+# Task 1b #
+###########
+
+identity = lambda x: x
+add1 = lambda x: x + 1
+sq = lambda x: x**2
+
+# (i) print(thrice(thrice)(add1)(6))
+# Explanation: The integer 33 would be printed. thrice(thrice) produces
+# a function of type (T' -> T') that will compose its input function
+# 3^3 (=27) times. As such, thrice(thrice)(add1) will produce a function
+# of type (F -> F) that adds 27 to its input number.
+# Ultimately, this means that thrice(thrice)(add1)(6) will be equivalent
+# to 27 + 6, which evaluates to 33.
+
+# (ii) print(thrice(thrice)(identity)(compose))
+# Explanation: thrice(thrice)(identity)(compose) is equivalent to the
+# function compose.
+#
+# The identity function simply returns its input without modifying it.
+# This means it will return the same input no matter how many times
+# it is composed. This means thrice(thrice)(identity) is
+# equivalent to identity. It follows that
+# thrice(thrice)(identity)(compose) is equivalent to identity(compose).
+# identity(compose) will then return compose.
+
+# (iii) print(thrice(thrice)(sq)(1))
+# Explanation: thrice(thrice)(sq) produces a function which returns
+# the result of n**2**2**2...(**2 27 times), where n is the float or int
+# that will be passed to the function as input.
+# When 1 is passed to this function, 1 will also be returned as
+# 1 raised to the power of any number will still be 1.
+
+# (iv) print(thrice(thrice)(sq)(2))
+# Explanation: Passing 2 to thrice(thrice)(sq) will produce the output
+# 2 to the power of 2 27 times. Given that this is an extremely big number,
+# the Python interpreter will take a long while before returning the result.
+
+
+###########
+# Task 2a #
+###########
+
+def combine(f, op ,n):
+    result = f(0)
+    for i in range(n):
+        result = op(result, f(i))
+        # FOR CHECKING THE VALUES OF i AND f(i)
+        # print(f"i = {i} |f({i}): {f(i)} | result = {result}")
+    return result
+
+def smiley_sum(t):
+    def f(x):
+        if x == 0:
+            # Edge case: if t == 1 and x == 0, the final result in combine
+            # would be f(0) + f(0). Since smiley_test(1) should be == 1,
+            # the only way I could think of to get this answer would be
+            # to bypass the equation and make f(0) 0.5 so that
+            # int(0.5 + 0.5) would become 1.
+            if t == 1:
+                return 0.5
+            else:
+                return (x-t)**2
+        elif x == t-1:
+            return 1
+        else:
+            return 2*((x-t)**2)
+
+    def op(x, y):
+        return int(x+y)
+
+    n = t
+
+    # Do not modify this return statement
+    return combine(f, op, n)
+
+
+"""
+# Note: I wanted to use this answer but wasn't sure if it'd be accepted
+# since I used an alternative return statement to account for the
+# smiley_test(1) edge case (even though the original return statement
+# technically wasn't modified)
+
+def smiley_sum(t):
+    def f(x):
+        # Since (x-t)**2 is a curve that passes through every square
+        # number in the series 1^2, 2^2,..., n^2 twice, it will pass
+        # through 1^2 1 more time than necessary.
+        #
+        # To be specific, 2 points in the curve (x-t)**2 will always be
+        # equal to 1: f(t-1) and f(t+1).
+        #
+        # Hence, we must remove 1 of these points if we want to get
+        # the correct result when calling smiley_sum(t) for some integer t.
+        # Otherwise, smiley_sum(t) will return the expected result + 1
+        if x == t+1:
+            return 0
+        else:
+            return (x-t)**2
+
+    def op(x, y):
+        return x+y
+
+    n  = t*2
+
+    # However, we will have to account for the edge case of smiley_sum(1) --
+    # if we remove f(t-1), smiley_sum(1) will return 0. On the other hand,
+    # if we remove f(t+1), smiley_sum(1) will return 2. This is because f(0)
+    # is added to the variable named result twice. In order to mitigate this,
+    # we will return 1 whenever smiley_sum(1) is called.
+    if t == 1:
+        return 1
+
+    # Do not modify this return statement
+    return combine(f, op, n)
+"""
+
+
+###########
+# Task 2b #
+###########
+
+def fib(n):
+    if n == 0 or n == 1:
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
+
+def new_fib(n):
+    def f(x):
+        if x == 0 or x == 1:
+            return x
+        elif n == x:
+            return -(f(n-1)-1)
+        else:
+            return f(x-1) + f(x-2)
+            
+
+    def op(x, y):
+        return x+y
+
+    return combine(f, op, n+1)
+
+"""
+Initial implementation:
+
+def new_fib(n):
+    def f(x):
+        if x == 0 or x == 1:
+            return x
+        elif n == x:
+            f_n = f(x-1) + f(x-2)
+            for i in range(n):
+                f_n -= f(i)
+            return f_n
+        else:
+            return f(x-1) + f(x-2)
+            
+
+    def op(x, y):
+        return x+y
+
+    return combine(f, op, n+1)
+"""
+
+
+
+# Your answer here:
+# Yes, it is possible to implement new_fib. In order to get the nth Fibonacci
+# number with the use of the combine function, the implementation can sum every
+# numbers from the 0th Fibonacci number to the nth Fibonacci number.
+# After the summation is complete, it must deduct every number from the 0th
+# Fibonacci number to the (n-1)th Fibonacci number.
+#
+# However, given that all terms in the sequence must be combined with the same
+# operation (op), the deduction must be recognised as a special case in the new_fib
+# function that should only take place after the calculation of the nth Fibonacci
+# number. This can be done by defining the helper function f in exactly the same
+# manner as the standard fib function before adding the special case.
+#
+# I have no idea how my new implementation works.
+
+
+# For testing
+
+successes = 0
+failures = 0
+    
+def function_test(f,n, expected_ans):
+    actual_ans = f(n)
+    test_passed = actual_ans == expected_ans
+    print(f"{f.__name__}({n}) == {expected_ans}: {test_passed}\n")
+    if test_passed:
+        global successes
+        successes += 1
+    else:
+        global failures
+        failures += 1
+
+def smiley_sum_test(n, expected_ans):
+    return function_test(smiley_sum, n, expected_ans)
+
+def new_fib_test(n, expected_ans):
+    return function_test(new_fib, n, expected_ans)
+
+def all_tests():
+    global successes
+    global failures
+    
+    test0 = smiley_sum_test(0,0)
+    test1 = smiley_sum_test(1,1)
+    test2 = smiley_sum_test(2,9)
+    test3 = smiley_sum_test(3,27)
+    test4 = smiley_sum_test(4,59)
+    test5 = smiley_sum_test(5,109)
+
+    test6 = new_fib_test(0,0)
+    test7 = new_fib_test(1,1)
+    test8 = new_fib_test(2,1)
+    test9 = new_fib_test(3,2)
+    test10 = new_fib_test(4,3)
+    test11 = new_fib_test(5,5)
+    test12 = new_fib_test(6,8)
+    test13 = new_fib_test(10,55)
+    test14 = new_fib_test(16,987)
+    test15 = new_fib_test(19,4181)
+    test16 = new_fib_test(31, 1346269)
+    print(f"{successes} tests passed, {failures} tests failed.")
+    
+    
+all_tests()
+
---
+++ Missions/missions/mission04/mission04-template.py
@@ -0,0 +1,48 @@
+#
+# CS1010X --- Programming Methodology
+#
+# Mission 4
+#
+# Note that written answers are commented out to allow us to run your
+# code easily while grading your problem set.
+
+from hi_graph import *
+
+##########
+# Task 1 #
+##########
+
+# (a)
+# your answer here
+
+# (b)
+# your answer here
+
+# (c)
+def vertical_line(point, length):
+    "your answer here"
+
+##draw_connected(200, vertical_line(make_point(0.1, 0.1), 0.4))
+
+# (d)
+# your answer here
+
+# (e)
+# your answer here
+
+##########
+# Task 2 #
+##########
+
+# (a)
+# your answer here
+
+# (b)
+def reflect_through_y_axis(curve):
+    def reflected_curve(t):
+        "your answer here"
+
+    return reflected_curve
+	
+##draw_connected_scaled(200, arc)
+##draw_connected_scaled(200, reflect_through_y_axis(arc))
