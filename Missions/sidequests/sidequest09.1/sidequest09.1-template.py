958a3593f92db60cb8326d052048a0b3ee921b4c
Isaac <42537694+ozervesh@users.noreply.github.com>
2023-03-13 23:14:49 +0800

    Update sidequest 9.1 submission files

--- Missions/sidequests/sidequest09.1/sidequest09.1-template.py
+++ Missions/sidequests/sidequest09.1/sidequest09.1-template.py
@@ -50,8 +50,46 @@ def module_prof(module):
 ###########
 
 def merge_lists(all_lst):
-    # Your code here
-    return []
+    """ Merges n sorted lists into 1 list
+    """
+    def is_list_empty(lst):
+        return len(lst) == 0
+    
+    def all_lists_empty():
+        for lst in all_lst:
+            if not is_list_empty(lst):
+                return False
+        return True
+    
+    def find_first_elements():
+        result = []
+        for lst in all_lst:
+            if not is_list_empty(lst):
+                result.append(lst[0])
+        return result
+
+    def remove_element_from_all_lst(elt):
+        """ Assumes the element to be removed is the 1st element
+        of a sublist within all_lst
+        """
+        for lst in all_lst:
+            if not is_list_empty(lst):
+                if elt == lst[0]:
+                    lst.remove(elt)
+                    break
+        return all_lst
+            
+    result = []
+
+    while not all_lists_empty():
+        first_elements = find_first_elements()
+        element_to_be_merged = min(first_elements)
+        # print(element_to_be_merged)
+        result.append(element_to_be_merged)
+        # print(f"all_lst: {all_lst}")
+        remove_element_from_all_lst(element_to_be_merged)
+
+    return result
 
 all_lst = [[2, 7, 10], [0, 4, 6], [3, 11]]
 print("## Q1a ##")
@@ -63,8 +101,48 @@ print(merge_lists(all_lst)) # [0, 2, 3, 4, 6, 7, 10, 11]
 ###########
 
 def merge(lists, field):
-    # Your code here
-    return []
+    """ Sorts module lists based on fields.
+    """
+    def is_list_empty(lst):
+        return len(lst) == 0
+    
+    def all_lists_empty():
+        for lst in lists:
+            if not is_list_empty(lst):
+                return False
+        return True
+    
+    def find_first_modules_in_sublists():
+        result = []
+        for lst in lists:
+            if not is_list_empty(lst):
+                first_module = lst[0]
+                result.append(first_module)
+        return result
+
+    def remove_mod_from_lists(target_mod):
+        """ Assumes the module to be removed is the 1st element
+        of a sublist within all_lst
+        """
+        for lst in lists:
+            if not is_list_empty(lst):
+                first_mod = lst[0]
+                if first_mod == target_mod:
+                    lst.remove(first_mod)
+                    break
+        return lists
+        
+    result = []
+
+    while not all_lists_empty():
+        first_modules_in_sublists = find_first_modules_in_sublists()
+        target_mod = min(first_modules_in_sublists,
+                         key=lambda module: field(module))
+        result.append(target_mod)
+        # print(f"result: {result}")
+        remove_mod_from_lists(target_mod)
+
+    return result
 
 
 list_of_lists = [[["CS1010S", "PROGRAMMING METHODOLOGY", "LEONG WING LUP, BEN"],
@@ -83,14 +161,44 @@ print(merge(list_of_lists, module_prof))
 ##########
 
 def merge_sort(lst, k, field):
-    # Your code here
-    return []
+    def divisible_by(a,b):
+        return a % b == 0
+    
+    def split_list(lst, k):
+        """ Splits lst into k sorted chunks
+
+        len(lst) will always >= 2
+
+        List[Module] -> List[List[Module]]
+        """
+        result = []
+        lst_len = len(lst)
+        lists_remaining = k
+        lower_index = 0
+        if divisible_by(lst_len, k):
+            next_list_len = lst_len // k       
+        else:
+            next_list_len = (lst_len // k) + 1
+        for _ in range(k):
+            upper_index = lower_index + next_list_len
+            next_list = lst[lower_index:upper_index]
+            next_list = merge_sort(next_list, k, field)
+            result.append(next_list)
+            lower_index = upper_index
+        return result
+
+    if len(lst) <= 1:
+        return lst
+    else:
+        sorted_sublists = split_list(lst, k)
+        result = merge(sorted_sublists, field)            
+        return result
 
 
 # For your own debugging
-# modules = read_json('modules_small.txt')
-# for module in merge_sort(modules, 2, module_code):
-#    print(module)
+modules = read_json('modules_small.txt')
+for module in merge_sort(modules, 2, module_prof):
+    print(module)
 
 
 ########### DO NOT REMOVE THE TEST BELOW ###########
@@ -135,6 +243,6 @@ def test(testfile_prefix):
     print(pass_k,"/", len(ks), " correct! Total time taken: ", total_time, " seconds.")
 
 print("## Q2 ##")
-# test('modules_small')
-# test('modules')
-# test('modules_empty')
+test('modules_small')
+test('modules')
+test('modules_empty')
